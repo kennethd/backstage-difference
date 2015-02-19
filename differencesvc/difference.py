567d151d59d337840c476d035057f907f23a1d61
Kenneth Dombrowski <kenneth@ylayali.net>
2015-02-19 10:57:27 -0500

    test responses for invalid type & value of number

--- differencesvc/difference.py
+++ differencesvc/difference.py
@@ -2,10 +2,18 @@ import math
 
 def sum_squares(n):
     """returns sum(1^2, ..., n^2)"""
+    try:
+        assert int(n) <= 100
+    except AssertionError:
+        raise ValueError("{} must be integer <= 100".format(n))
     return sum([ i * i for i in xrange(1, n + 1) ])
 
 def square_sums(n):
     """returns sum([1, ..., n])^2"""
+    try:
+        assert int(n) <= 100
+    except AssertionError:
+        raise ValueError("{} must be integer <= 100".format(n))
     return sum([ i for i in xrange(1, n + 1) ]) ** 2
 
 def difference(n):
--- differencesvc/test/test_difference.py
+++ differencesvc/test/test_difference.py
@@ -7,10 +7,34 @@ class TestDifference(unittest.TestCase):
 
     def test_sum_squares(self):
         self.assertEqual(difference.sum_squares(10), 385)
+        # enforces n <= 100
+        with self.assertRaises(ValueError) as ctx:
+            difference.sum_squares(101)
+        self.assertEqual(ctx.exception.message, "101 must be integer <= 100")
+        # enforces n is int
+        with self.assertRaises(ValueError) as ctx:
+            difference.sum_squares('a')
+        self.assertEqual(ctx.exception.message, "invalid literal for int() with base 10: 'a'")
 
     def test_square_sums(self):
         self.assertEqual(difference.square_sums(10), 3025)
+        # enforces n <= 100
+        with self.assertRaises(ValueError) as ctx:
+            difference.square_sums(101)
+        self.assertEqual(ctx.exception.message, "101 must be integer <= 100")
+        # enforces n is int
+        with self.assertRaises(ValueError) as ctx:
+            difference.square_sums('a')
+        self.assertEqual(ctx.exception.message, "invalid literal for int() with base 10: 'a'")
 
     def test_difference(self):
         self.assertEqual(difference.difference(10), 2640)
+        # enforces n <= 100
+        with self.assertRaises(ValueError) as ctx:
+            difference.difference(101)
+        self.assertEqual(ctx.exception.message, "101 must be integer <= 100")
+        # enforces n is int
+        with self.assertRaises(ValueError) as ctx:
+            difference.difference('a')
+        self.assertEqual(ctx.exception.message, "invalid literal for int() with base 10: 'a'")
 
